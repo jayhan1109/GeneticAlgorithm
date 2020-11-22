5e3e8d078296a2e37bd93d28f448449fa3a7592b
jayhan1109 <jayhan1109@gmail.com>
2020-11-22 15:23:02 +0900

    Implemented population class

---
+++ population.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Jeongho Han on 11/22/2020.
+//
+
+#include "tour.hpp"
+
+class population {
+
+public:
+    const unsigned long POPULATION_SIZE = 32;
+    vector<tour> population{POPULATION_SIZE};
+
+};
+
